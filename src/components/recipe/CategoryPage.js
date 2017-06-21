bc5d33ce2e471b253ac4e4de2ca7fee5d1898886
Elena Sufieva <fiolent06@gmail.com>
2017-06-21 16:51:07 +0200

    Make CategoryPage render corresponding recipes

--- src/components/recipe/CategoryPage.js
+++ src/components/recipe/CategoryPage.js
@@ -1,23 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import store from '../../store';
+import Gallery from '../common/RecipesGallery';
 
 
 class CategoryPage extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      category: this.props.match.params.category,
+      recipes: this.props.recipes
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const nextCategory = nextProps.match.params.category;
+    this.setState({ category: nextCategory.slice()}, () => {
+      const recipes = store.getState().recipes;
+      let newRecipes = recipes.filter(recipe => {
+        return recipe.category.toLowerCase() == this.state.category;
+      });
+      this.setState({ recipes: newRecipes });
+    });
   }
 
   render() {
-    const category = this.props.match.params.category;
-    return (
-      <div>
-      This is {category} page
-      </div>);
+    const props = this.props;
+    const category = this.state.category;
+
+    if ( this.props.isLoading ) {
+         return (<div>Loading...</div>);
+    }
+    return (<Gallery recipes={this.state.recipes} category={category} />);
   }
 }
 
 CategoryPage.propTypes = {
-
+  recipes: PropTypes.array,
+  isLoading: PropTypes.number
 };
 
-export default CategoryPage;
+const mapStateToProps = (state, ownProps) => {
+  const category = ownProps.match.params.category;
+  const getCategorizedRecipes = (category) => {
+    return state.recipes.filter(recipe => {
+      return recipe.category.toLowerCase() == category;
+    });
+  };
+  const recipes = getCategorizedRecipes(category);
+  return {
+    isLoading: state.isLoading,
+    recipes: recipes
+  };
+};
+
+export default connect(mapStateToProps)(CategoryPage);
