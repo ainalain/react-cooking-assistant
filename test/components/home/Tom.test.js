6e894c3e7614927ab6ce18be15f7f8bad3ddd465
Elena Sufieva <fiolent06@gmail.com>
2017-07-13 08:50:49 +0200

    Add unit test for Tom component

---
+++ test/components/home/Tom.test.js
@@ -0,0 +1,11 @@
+import expect from 'expect';
+import React from 'react';
+import { shallow  } from 'enzyme';
+import Greetings from '../../../src/components/home/Tom';
+
+describe('Tom component', () => {
+  it('renders an svg', () => {
+    const component = shallow(<Greetings />);
+    expect(component.find('svg').length).toBe(1);
+  });
+});
